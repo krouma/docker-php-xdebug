3e6fc0204a23a95927a41f280504e8efa25ac052
Matyáš Kroupa <kroupa.matyas@gmail.com>
2018-11-13 21:44:57 +0100

    Edit build script

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-for version in 5.6 7.0 7.1 7.1-nette 7.2 7.2-nette; do
-    docker build -t milk/php-xdebug:${version} ${version}
+MINOR_VERSIONS=7.1 7.2
+
+for version in $MINOR_VERSIONS; do
+    #update base container version and save it to fullversion
+    echo "\nGetting latest version of PHP ${version}\n"
+    docker pull php:${version}
+    fullversion=$(docker run -a stdout --rm php:${version}-apache php --version | head -1 | awk '{print $2}')
+
+    #build containers
+    echo "\nBuilding version ${fullversion}\n"
+    docker build -t krouma/php-xdebug:${version} ${version} --no-cache
+    docker build -t krouma/php-xdebug:${version}-nette ${version}-nette --no-cache
+
+    #tag fullversion
+    echo "\nTagging ${version} as ${fulversion}\n"
+    docker tag krouma/php-xdebug:${version} krouma/php-xdebug{$fullversion}
+    docker tag krouma/php-xdebug:${version}-nette krouma/php-xdebug{$fullversion}-nette
 done
