b2944fb47316f8f33f3db55d81a5f3889bf1fee3
Matyáš Kroupa <kroupa.matyas@gmail.com>
2018-11-13 21:16:22 +0100

    Use printf instead of echo

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@ MINOR_VERSIONS=(7.1 7.2)
 
 for version in ${MINOR_VERSIONS[*]}; do
     #update base container version and save it to fullversion
-    echo "\nGetting latest version of PHP ${version}\n"
+    printf "\nGetting latest version of PHP ${version}\n"
     docker pull php:${version}
     fullversion=$(docker run -a stdout --rm php:${version}-apache php --version | head -1 | awk '{print $2}')
 
     #build containers
-    echo "\nBuilding version ${fullversion}\n"
+    printf "\nBuilding version ${fullversion}\n"
     docker build -t krouma/php-xdebug:${version} ${version} --no-cache
 
     #tag fullversion
-    echo "\nTagging ${version} as ${fulversion}\n"
+    printf "\nTagging ${version} as ${fulversion}\n"
     docker tag krouma/php-xdebug:${version} krouma/php-xdebug{$fullversion}
 done
