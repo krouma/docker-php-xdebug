93a6368f02ad4ad38d461cd19d41883e8b44231d
Matyáš Kroupa <kroupa.matyas@gmail.com>
2018-11-24 13:52:41 +0100

    Remove nette variants from generate.sh

--- generate.sh
+++ generate.sh
@@ -5,9 +5,7 @@ apcu_version=(
 	[5.6]='apcu-4.0.11'
 	[7.0]='apcu-stable'
 	[7.1]='apcu-stable'
-	[7.1-nette]='apcu-stable'
 	[7.2]='apcu-stable'
-	[7.2-nette]='apcu-stable'
 )
 
 declare -A xdebug_version
@@ -15,9 +13,7 @@ xdebug_version=(
 	[5.6]='xdebug-stable'
 	[7.0]='xdebug-stable'
 	[7.1]='xdebug-stable'
-	[7.1-nette]='xdebug-stable'
 	[7.2]='xdebug-stable'
-	[7.2-nette]='xdebug-stable'
 )
 
 declare -A extensions_dir
@@ -25,9 +21,7 @@ extensions_dir=(
 	[5.6]='no-debug-non-zts-20131226'
 	[7.0]='no-debug-non-zts-20151012'
 	[7.1]='no-debug-non-zts-20160303'
-	[7.1-nette]='no-debug-non-zts-20160303'
 	[7.2]='no-debug-non-zts-20170718'
-	[7.2-nette]='no-debug-non-zts-20170718'
 )
 
 for version in 5.6 7.0 7.1 7.1-nette 7.2 7.2-nette; do
